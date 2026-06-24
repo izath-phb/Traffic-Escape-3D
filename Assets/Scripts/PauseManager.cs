9b4a018571bdf7f87dddcc7255ce6ae8d7fed691
izath-phb <izathbsl123@gmail.com>
2026-06-24 18:35:27 +0700

    Update Terbaru

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -4,11 +4,20 @@ public class PauseManager : MonoBehaviour
 {
     public GameObject pausePanel;
     public GameObject pauseButton;
+    public GameObject joystick;
+    public GameObject nitroButton;
 
     public void Pause()
     {
         pausePanel.SetActive(true);
         pauseButton.SetActive(false);
+
+        if (joystick != null)
+            joystick.SetActive(false);
+
+        if (nitroButton != null)
+            nitroButton.SetActive(false);
+
         Time.timeScale = 0f;
     }
 
@@ -16,6 +25,13 @@ public class PauseManager : MonoBehaviour
     {
         pausePanel.SetActive(false);
         pauseButton.SetActive(true);
+
+        if (joystick != null)
+            joystick.SetActive(true);
+
+        if (nitroButton != null)
+            nitroButton.SetActive(true);
+
         Time.timeScale = 1f;
     }
 }
